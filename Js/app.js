fc467c5e7586042f717af8cce27c1965e206c525
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-05-11 14:54:26 +0200

    :birthday:

--- Js/app.js
+++ Js/app.js
@@ -1,28 +1,9 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 270 || document.documentElement.scrollTop > 270) {
-    document.querySelector(".astronaut").style.opacity = "1";
-    document.querySelector(".astronaut").style.top = "90%";
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    document.querySelector(".header").style.backgroundColor = "rgba(0, 0, 0, 0.9)"
   } else {
-    document.querySelector(".astronaut").style.opacity = "0";
-    document.querySelector(".astronaut").style.top = "120%";
+    document.querySelector(".header").style.backgroundColor = "rgba(0, 0, 0, 0)"
   }
 }
-
-
-// var text = ['“What did the 5 fingers say to the face. S L A P!”', '“Hey hey hey, smoke weed everyday”'];
-
-// textSequence(0);
-// function textSequence(i) {
-
-//     if (text.length > i) {
-//         setTimeout(function() {
-//             document.getElementById("sequence").innerHTML = text[i];
-//             textSequence(++i);
-//         }, 2500);
-
-//     } else if (text.length == i) {
-//         textSequence(0);
-//     }
-// }
