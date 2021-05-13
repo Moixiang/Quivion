17e58ac2b141f9a5899a39a606eecd73ef4a362a
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-05-14 00:17:11 +0200

    :+1:

--- Js/scroll.js
+++ Js/scroll.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 670 || document.documentElement.scrollTop > 670) {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
     document.getElementById("header").style.opacity = "1";
     document.getElementById("header").style.top = "0";
   } else {
