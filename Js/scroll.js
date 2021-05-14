93538a3102445ad21703ba0c1bfb99625fa374d4
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-05-14 14:43:57 +0200

    Header

--- Js/scroll.js
+++ Js/scroll.js
@@ -2,12 +2,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
-    document.getElementById("header").style.height = "7vh";
-    document.querySelector(".logo").style.fontSize = "2rem";
-    document.querySelector(".navigation").style.fontSize = "larger";
-  } else {
+    
     document.getElementById("header").style.height = "5vh";
     document.querySelector(".logo").style.fontSize = "1.5rem";
     document.querySelector(".navigation").style.fontSize = "smaller";
+  } else {
+    document.getElementById("header").style.height = "7vh";
+    document.querySelector(".logo").style.fontSize = "1.9rem";
+    document.querySelector(".navigation").style.fontSize = "larger";
   }
 }
