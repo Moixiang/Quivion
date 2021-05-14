cc4ccdd5141d34e27b56504a2b8019e1415463c6
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-05-14 14:09:58 +0200

    :sunglasses:

--- Js/scroll.js
+++ Js/scroll.js
@@ -1,11 +1,13 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    document.getElementById("header").style.opacity = "1";
-    document.getElementById("header").style.top = "0";
+  if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
+    document.getElementById("header").style.height = "7vh";
+    document.querySelector(".logo").style.fontSize = "2rem";
+    document.querySelector(".navigation").style.fontSize = "larger";
   } else {
-    document.getElementById("header").style.opacity = "0";
-    document.getElementById("header").style.top = "-10%";
+    document.getElementById("header").style.height = "5vh";
+    document.querySelector(".logo").style.fontSize = "1.5rem";
+    document.querySelector(".navigation").style.fontSize = "smaller";
   }
 }
