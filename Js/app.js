03eb713001294a0e73c7bf5e21f262b05785276e
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-05-14 13:42:24 +0200

    :ant:

--- Js/app.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    document.querySelector(".header").style.backgroundColor = "rgba(0, 0, 0, 0.9)"
-  } else {
-    document.querySelector(".header").style.backgroundColor = "rgba(0, 0, 0, 0)"
-  }
-}
